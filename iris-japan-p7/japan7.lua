31292925de1db670da9a3ff4d12be10e0de8ce25
SteamsDev <dev208@msn.com>
2023-01-20 13:56:09 -0500

    Added Mission 7
    Added Mission 7 of the Japan Campaign (the series finale)

---
+++ iris-japan-p7/japan7.lua
@@ -0,0 +1,31 @@
+WorldLoaded = function()
+    Penta = Player.GetPlayer("Penta")
+    Nitro = Player.GetPlayer("Nitro")
+    DA = Player.GetPlayer("DA")
+    Shade = Player.GetPlayer("Shade")
+    Penta.Cash = 12000
+    Camera.Position = Actor236.CenterPosition
+
+    Media.DisplayMessage("This is the day we finish the DA. Take the HQ!")
+    DestroyObjective = Penta.AddPrimaryObjective("Capture or destroy the DA Headquarters.")
+    NitroObjective = Penta.AddSecondaryObjective("Destroy the Nitro base.")
+    ShadeObjective = Penta.AddSecondaryObjective("Destroy the Shade base.")
+
+    Trigger.OnKilledOrCaptured(DAHQ, function()
+        Penta.MarkCompletedObjective(DestroyObjective)
+    end)
+end
+
+Tick = function()
+    if Nitro.HasNoRequiredUnits() then
+        Penta.MarkCompletedObjective(NitroObjective)
+    end
+
+    if Shade.HasNoRequiredUnits() then
+        Penta.MarkCompletedObjective(ShadeObjective)
+    end
+
+    if Penta.HasNoRequiredUnits() then
+        Penta.MarkFailedObjective(DestroyObjective)
+    end
+end
