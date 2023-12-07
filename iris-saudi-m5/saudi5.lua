08e519c53b32beb94963c695562be1ebfb399c49
SteamsDev <dev208@msn.com>
2023-12-06 22:38:34 -0500

    Mission 5 Uploaded
    Mission 5 has been uploaded.
    
    I have also verified and confirmed that all levels work with version 1.0. No patches should be required.
    
    Please report any problems to me if you see them! Thanks!

---
+++ iris-saudi-m5/saudi5.lua
@@ -0,0 +1,31 @@
+OilProtected = true
+OilDerricks = {Actor34, Actor35, Actor36, Actor37, Actor38, Actor65, Actor73, Actor84, Actor85, Actor86}
+
+WorldLoaded = function()
+    Shade = Player.GetPlayer("Shade")
+    Penta = Player.GetPlayer("Penta")
+    DA = Player.GetPlayer("DA")
+    Penta.Cash = 85000
+
+    OilNumber = #OilDerricks
+
+    ProtectObjective = Shade.AddPrimaryObjective("Keep at least four of the oil derricks intact.")
+    DestroyObj = Shade.AddPrimaryObjective("Drive out the Penta.")
+end
+
+Tick = function()
+    if (Penta.HasNoRequiredUnits()) then
+        Shade.MarkCompletedObjective(DestroyObj)
+        Shade.MarkCompletedObjective(ProtectObjective)
+    end
+
+    OilKilled = 0
+    for i = 1, OilNumber do
+        if OilDerricks[i].IsDead then
+            OilKilled = OilKilled + 1
+        end
+    end
+    if OilNumber - OilKilled < 4 then
+        Shade.MarkFailedObjective(ProtectObjective)
+    end
+end
